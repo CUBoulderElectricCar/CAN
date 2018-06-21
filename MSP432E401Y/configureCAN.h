66b2b3def94b8948a1c0f06811520d2d42e8a360
Jack Danielski <jackdski555@gmail.com>
2018-06-20 20:26:36 -0600

    Init configure.h

---
+++ MSP432E401Y/configureCAN.h
@@ -0,0 +1,19 @@
+/*
+ *
+ * configure.h
+ * Author: Jack Danielski
+ * June 2018
+ *
+ */
+
+/*
+ * configure CAN interface
+ */
+
+#include "ti/devices/msp432e4/driverlib/driverlib.h"
+
+//#include "msp.h"
+#include <stdint.h>
+#include <stdbool.h>
+
+void configureCAN();
